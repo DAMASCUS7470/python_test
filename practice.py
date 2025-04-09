4fed247c7acfed9fa32e242dccaefce49d060544
DAMASCUS7470 <damascus7470@gmail.com>
2025-04-09 00:25:02 -0400

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 def in_autotests_we_trust(a1, b1):
     if a1 == b1:
-        print('Test passed')
+        print('PASSED')
     else:
-        print('Test failed')
+        print('FAILED')
 
 in_autotests_we_trust(True, 0)
 
