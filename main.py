35e05a43d11da7c8ce9b37130dfdd0a8ad4fd401
DAMASCUS7470 <pujatr@yahoo.co.in>
2025-04-08 23:55:58 -0400

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def in_autotests_we_trust(a, b):
-    if a == b:
+def in_autotests_we_trust(x, y):
+    if x == y:
         print('Test passed')
     else:
         print('Test failed')
 
-in_autotests_we_trust('10', 10)
+in_autotests_we_trust('10', '10')
 
 in_autotests_we_trust(0, False)
